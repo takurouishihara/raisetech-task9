92554d26878f08a5724a10f3aaf497698d5b8b5b
takurouishihara <k19008@std.nagaokauniv.ac.jp>
2022-11-12 19:13:55 +0900

    MyBatisを使ったREAD処理を実装

--- src/main/java/net/raisetech/raisetechtask9/MovieController.java
+++ src/main/java/net/raisetech/raisetechtask9/MovieController.java
@@ -1,6 +1,7 @@
 package net.raisetech.raisetechtask9;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +19,8 @@ public class MovieController {
     public List<Movie> getMovies(){
         List<Movie> movies = movieMapper.findAll();
         return movies;
+    }
+
 
     }
-}
+
--- src/main/java/net/raisetech/raisetechtask9/MovieMapper.java
+++ src/main/java/net/raisetech/raisetechtask9/MovieMapper.java
@@ -1,9 +1,10 @@
 package net.raisetech.raisetechtask9;
 
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-
 import java.util.List;
+
 
 @Mapper
 public interface MovieMapper {
